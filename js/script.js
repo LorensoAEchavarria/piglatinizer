1a8227ef45cebeace21e092c6751b5cabd2634ff
LorensoAEchavarria <Lorensoaechavarria@gmail.com>
2019-10-17 22:05:54 +0000

    input is complete, now fix the bug w input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("#button").click(function(){
+    var input= $(".input").val
+    $("body").append("<p>" + input + "</p>")
+
+    });
 
 
 
-  
-  
-  
-  
+
+
+
 });
